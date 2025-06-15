175860e56ddacb8ed1776731b6c81f0be59a2be7
KerubinZero~ <vv22015@ues.edu.sv>
2025-06-15 17:17:05 -0600

    Parte 2 Completa

--- resources/views/backend/admin/dashboard/apis.blade.php
+++ resources/views/backend/admin/dashboard/apis.blade.php
@@ -81,4 +81,4 @@
 
 <!-- Agregar en el <head> -->
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
-<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script> d 
